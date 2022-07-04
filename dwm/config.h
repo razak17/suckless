939e00c24de6f9619081bea52289e8a7c110ee6a
Razak <moabdulrazak17@gmail.com>
2022-07-04 21:23:03 +0000

    chore(mappings): update firefox profiles name

--- dwm/config.h
+++ dwm/config.h
@@ -98,7 +98,7 @@ static const Layout layouts[] = {
 // commands
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]         = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont};
-static const char *firefox[]          = { "ifirefox", NULL };
+static const char *firefox[]          = { "ifirefox-profiles", NULL };
 static const char *filecmd[]          = { "thunar", NULL };
 static const char *mailcmd[]          = { "thunderbird", NULL };
 static const char *termcmd[]          = { "kitty", NULL };
