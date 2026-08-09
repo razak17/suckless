8984768d11cc17ceca73ad01ab11cc15fb5fee44
Abdul Razak <moabdulrazak17@gmail.com>
2026-08-09 17:48:27 +0000

    feat(st): set font size properly

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:pixelsize=11:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "JetBrains Mono Nerd Font:size=9:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:size=9:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
